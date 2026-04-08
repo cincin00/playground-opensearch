0adf2a6f09929e3bd0e6cd3cc6d7e44a622cf251
gs1902840 <ycg@gabiacns.com>
2026-04-08 14:31:20 +0900

    TypeScript와 ESLint 설정을 정비한다

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,42 @@
-import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['coverage', 'dist', 'node_modules'],
+    ignores: ['coverage', 'dist', 'node_modules', 'eslint.config.mjs'],
   },
-  js.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettierRecommended,
   {
     files: ['**/*.ts'],
     languageOptions: {
       globals: {
-        ...globals.jest,
         ...globals.node,
       },
+      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
+  },
+  {
     rules: {
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports' },
-      ],
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
-      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
   {
     files: ['**/*.spec.ts', 'test/**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
     },
   },
-  eslintConfigPrettier,
 );
